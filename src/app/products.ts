0f74258b4cbd2ca6ec2b4d286c678e4b454c799a
alladan <allan@alladan.net>
2019-09-04 13:53:32 -0400

    Initial Commit
    G4 Source Code

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Eat a Cake',
+    date: "1/1/19",
+    description: 'Remember to eat a cake once in a while'
+  },
+  {
+    name: 'Remember Birthday',
+    date: "12/25/1969",
+    description: 'Super hard to remember your Birthday sometimes...'
+  },
+  {
+    name: 'Finish CMSC495 Capstone Project',
+    date: "Sometime in October",
+    description: 'What are we doing again?'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
